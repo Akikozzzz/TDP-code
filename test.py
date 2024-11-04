d0f839c5eccf6465059e5ee008c76f7ab28c1771
Akikozzzz <75301144+Akikozzzz@users.noreply.github.com>
2024-11-05 07:40:02 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ class Nao(Robot):
             self.forwards = Motion('../../motions/Forwards50.motion')
             self.turnLeft40 = Motion('../../motions/TurnLeft40.motion')
             self.turnRight40 = Motion('../../motions/TurnRight40.motion')
+            self.Shoot = Motion('../../motions/Shoot.motion')
             if self.forwards is None or self.turnLeft60 is None or self.turnRight60 is None or self.stand is None:
                 print("Error: Motion files could not be loaded. Check file paths.")
         except Exception as e:
@@ -58,24 +59,20 @@ class Nao(Robot):
 
     def calculateDistance(self, pos1, pos2):
         # 计算两点之间的距离
-        return math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[2] - pos2[2]) ** 2)
+        return math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)
 
     def calculateAngle(self, pos1, pos2):
         # 计算当前点和目标点之间的角度
         dx = pos2[0] - pos1[0]
-        dz = pos2[2] - pos1[2]
+        dz = pos2[1] - pos1[1]
         return math.atan2(dz, dx)
 
-    def run(self, target_x, target_z):
-        target_position = (target_x, 0, target_z)  # 目标位置 (x, y, z)
+    def run(self, target_x, target_y):
+        target_position = (target_x, target_y, 0)  # 目标位置 (x, y, z)
 
         while True:
             robot_position = self.get_position()
             distance_to_target = self.calculateDistance(robot_position, target_position)
-
-            if distance_to_target < 0.2:  # 如果距离小于设定的范围，停止
-                print("到达目标位置")
-                break
 
             # 计算当前与目标位置的角度差
             angle_to_target = self.calculateAngle(robot_position, target_position)
@@ -95,14 +92,22 @@ class Nao(Robot):
                 # 角度接近目标方向，开始前进
                 self.startMotion(self.forwards)
                 
+            if distance_to_target < 0.2:  # 如果距离小于设定的范围，停止
+                print("到达目标位置")
+                # 执行踢球动作
+                self.startMotion(self.Shoot)
+
+                print("踢球动作完成")
+                break
+                
             if self.step(self.timeStep) == -1:
                 break
 
 # 创建Nao实例并运行主循环
 robot = Nao()
 # 指定目标坐标
-target_x, target_z = -1.3, 0.3  # 示例目标坐标
-robot.run(target_x, target_z)
+target_x, target_y = 0.45, 0.08  # 示例目标坐标
+robot.run(target_x, target_y)
 
 
 
